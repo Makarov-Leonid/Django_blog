a725b1e3636b22c919bed7f0ef3134126fa4680f
Leo <4.leo.makarov@gmail.com>
2019-06-14 15:26:56 +0300

    add page navigatin

--- Django_blog/blog_eng/blog/views.py
+++ Django_blog/blog_eng/blog/views.py
@@ -9,12 +9,19 @@ from .models import Post, Tag
 from .utils import *
 from .forms import TagForms as TagForm
 from .forms import PostForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all()
-    n = ["oleg","Sasha"]
-    return render(request, 'blog/index.html', context = {'posts':posts})
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    return render(request, 'blog/index.html', context = {'page_obj':page})
 
 def tag_list(request):
     tags = Tag.objects.all()
@@ -28,30 +35,36 @@ class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = "blog/tag_detail.html"
 
-class TagCreate(ObjCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
+    raise_exception = True
 
-class PostCreate(ObjCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create_form.html'
+    raise_exception = True
 
-class TagUpdate(ObjUpdateMixin, View):
+class TagUpdate(LoginRequiredMixin, ObjUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
+    raise_exception = True
 
-class PostUpdate(ObjUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjUpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'blog/post_update_form.html'
+    raise_exception = True
 
-class TagDelete(ObjDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjDeleteMixin, View):
     model = Tag
     template = 'blog/tag_delete_form.html'
     redirect_url = 'tag_list_url'
+    raise_exception = True
 
-class PostDelete(ObjDeleteMixin, View):
+class PostDelete(LoginRequiredMixin, ObjDeleteMixin, View):
     model = Post
     template = 'blog/post_delete_form.html'
     redirect_url = 'posts_list_url'
+    raise_exception = True
